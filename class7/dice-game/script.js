4126f02d18cc1ba5e861af740e9af71c04c54a09
Chao Zhang <raidenchao@gmail.com>
2020-11-08 20:38:43 -0800

    update

--- class7/dice-game/script.js
+++ class7/dice-game/script.js
@@ -5,6 +5,15 @@ function start() {
   // change the images
   $('#img1').attr('src', './dice' +  num1  + '.png');
   $('#img2').attr('src', './dice' +  num2  + '.png');
+
+  if (num1 > num2) {
+    $('h1').text(' 🚩 Player 1 win !')
+  } else if (num2 > num1) {
+    $('h1').text('Player 2  win ! 🚩')
+  } else {
+    $('h1').text('Draw !')
+  }
+
 }
 
 
---
+++ class8/project3-drum-kit/script.js
@@ -0,0 +1,24 @@
+$('#w').click(function() {
+  let audio = new Audio('./sounds/tom-1.mp3')
+  audio.play()
+})
+
+$('#a').click(function() {
+  let audio = new Audio('./sounds/tom-2.mp3')
+  audio.play()
+})
+
+$('#s').click(function() {
+  let audio = new Audio('./sounds/tom-3.mp3')
+  audio.play()
+})
+
+$('#d').click(function() {
+  let audio = new Audio('./sounds/tom-4.mp3')
+  audio.play()
+})
+
+$('#j').click(function() {
+  let audio = new Audio('./sounds/crash.mp3')
+  audio.play()
+})
