d2b2314277535578ed7c8d07e88364f99341102f
Chao Zhang <raidenchao@gmail.com>
2020-11-15 14:29:55 -0800

    udpate drum kit

--- class8/project3-drum-kit/script.js
+++ class8/project3-drum-kit/script.js
@@ -22,3 +22,44 @@ $('#j').click(function() {
   let audio = new Audio('./sounds/crash.mp3')
   audio.play()
 })
+
+document.addEventListener('keypress', function(e) {
+
+  if (e.key === 'w') {
+    let audio = new Audio('./sounds/tom-1.mp3')
+    audio.play()
+  }
+
+  if (e.key === 'a') {
+    let audio = new Audio('./sounds/tom-2.mp3')
+    audio.play()
+  }
+
+  if (e.key === 's') {
+    let audio = new Audio('./sounds/tom-3.mp3')
+    audio.play()
+  }
+
+  if (e.key === 'd') {
+    let audio = new Audio('./sounds/tom-4.mp3')
+    audio.play()
+  }
+
+  if (e.key === 'j') {
+    let audio = new Audio('./sounds/crash.mp3')
+    audio.play()
+  }
+
+  if (e.key === 'k') {
+    let audio = new Audio('./sounds/kick-bass.mp3')
+    audio.play()
+  }
+
+  if (e.key === 'l') {
+    let audio = new Audio('./sounds/snare.mp3')
+    audio.play()
+  }
+
+})
+
+
